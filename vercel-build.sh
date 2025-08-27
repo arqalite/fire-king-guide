3759ee5f3400c775a43b82bb3cfe54c4dafbc9d6
Antonio Curavalea <one.kyonblack@gmail.com>
2025-08-27 20:49:43 +0300

    fix(build): use Vercel branch env var

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,7 +3,7 @@
 curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.43/mdbook-v0.4.43-x86_64-unknown-linux-musl.tar.gz;
 tar -xvzf mdbook.tar.gz; 
 
-sed -i "s#{VERSION}#$(git rev-parse --abbrev-ref HEAD)#g" theme/index.hbs
+sed -i "s#{VERSION}#$VERCEL_GIT_COMMIT_REF#g" theme/index.hbs
 sed -i "s#{DATE}#$(printf '%(%Y-%m-%d)T\n' -1)#g" theme/index.hbs
 
 ./mdbook build
